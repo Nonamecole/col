63ccf7ce4cc88a7c0f6e113c5d38ffaebdf39c98
Felix Jumason <felixkent360@gmail.com>
2023-05-14 11:31:21 +0300

    updated information.jsx

--- src/pages/Infomation.jsx
+++ src/pages/Infomation.jsx
@@ -27,10 +27,10 @@ function Information() {
       <p>Click on the buttons below to watch some informative videos:</p>
       <div>
         <button style={buttonStyle}>
-          <a href={video1} target="_blank" rel="noopener noreferrer" style={linkStyle}>Watch Video 1</a>
+          <a href={video1} target="_blank" rel="noopener noreferrer" style={linkStyle}>E-Poultry app</a>
         </button>
         <button style={buttonStyle}>
-          <a href={video2} target="_blank" rel="noopener noreferrer" style={linkStyle}>Watch Video 2</a>
+          <a href={video2} target="_blank" rel="noopener noreferrer" style={linkStyle}>ESmart Brooder app</a>
         </button>
       </div>
     </div>
