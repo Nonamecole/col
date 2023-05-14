ad0552470a1a59e701544ed2a7947b854c575ead
olivebishop <olivehendrilgen1@gmail.com>
2023-05-14 08:01:57 +0300

    added servies page

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,5 +1,10 @@
-import React from 'react'
-import './css/Service.css'
+import React from 'react';
+import './css/Service.css';
+import service1 from '../assets/service1.webp';
+import service2 from '../assets/service2.jpg';
+import service3 from '../assets/service3.webp';
+import service4 from '../assets/service4.svg';
+
 const Service = () => {
   return (
     <div className="service-section">
@@ -8,6 +13,7 @@ const Service = () => {
         <div className="col-md-6">
           <div className="service-card">
             <div className="service-card-content">
+              <img src={service1} alt="Training" className="service-card-icon" />
               <h3 className="service-card-title">Training & Capacity Building</h3>
             </div>
           </div>
@@ -15,6 +21,7 @@ const Service = () => {
         <div className="col-md-6">
           <div className="service-card">
             <div className="service-card-content">
+              <img src={service2} alt="Farm Visit" className="service-card-icon" />
               <h3 className="service-card-title">Farm Visit & Housing Quotation</h3>
             </div>
           </div>
@@ -22,6 +29,7 @@ const Service = () => {
         <div className="col-md-6">
           <div className="service-card">
             <div className="service-card-content">
+              <img src={service3} alt="Production Costing" className="service-card-icon" />
               <h3 className="service-card-title">Production Costing and Budget</h3>
             </div>
           </div>
@@ -29,6 +37,7 @@ const Service = () => {
         <div className="col-md-6">
           <div className="service-card">
             <div className="service-card-content">
+              <img src={service4} alt="Farm Support" className="service-card-icon" />
               <h3 className="service-card-title">Farm Support and Technical Assistance</h3>
             </div>
           </div>
